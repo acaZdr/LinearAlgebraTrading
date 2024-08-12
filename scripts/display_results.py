66ce96acc96713717a75b84dfcd39c67f783bf18
njari <n.jaric03@gmail.com>
2024-08-12 12:49:31 +0200

    Added scripts directory

---
+++ scripts/display_results.py
@@ -0,0 +1,5 @@
+from src.utils.data_saving_and_displaying import display_npy_file
+
+
+display_npy_file('../results/outputs/test_actuals.npy')
+display_npy_file('../results/outputs/test_predictions.npy')
