05026ca5637929edc641776a3d89de750ff5e337
njari <n.jaric03@gmail.com>
2024-08-13 14:49:00 +0200

    Further improved lin_regression.py, added lstm_dyn_attention.py based on volatility, made crypto_dataset.py into separate file

---
+++ logic/models/crypto_dataset.py
@@ -0,0 +1,15 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class CryptoDataset(Dataset):
+    def __init__(self, data, seq_length):
+        self.data = torch.FloatTensor(data)
+        self.seq_length = seq_length
+
+    def __len__(self):
+        return len(self.data) - self.seq_length
+
+    def __getitem__(self, idx):
+        return (self.data[idx:idx + self.seq_length, :-1],
+                self.data[idx + self.seq_length - 1, -1])
